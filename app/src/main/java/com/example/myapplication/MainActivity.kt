401a70c2c4a1192caf29f7d6d158e6430c7910b5
xbit <baha.kasimov2005@gmail.com>
2023-03-25 19:28:52 +0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val largeAnimation = AnimationUtils.loadAnimation(this,R.anim.large)
+
+
+    }
+}
